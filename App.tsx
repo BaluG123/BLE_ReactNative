25b1290b5b55c116a7d1fef93babb625b56db77c
BaluG123 <balu1072000@gmail.com>
2024-05-21 17:10:22 +0530

    connecting and displaying the available data using service,characteristic UUID's

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, StyleSheet, PermissionsAndroid, Platform } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, PermissionsAndroid, Platform,TouchableOpacity,SafeAreaView } from 'react-native';
 import BleManager from 'react-native-ble-manager';
 import { NativeEventEmitter, NativeModules } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { Buffer } from 'buffer';
 
 const BleManagerModule = NativeModules.BleManager;
 const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
@@ -10,6 +11,9 @@ const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 const App = () => {
   const [isScanning, setIsScanning] = useState(false);
   const [peripherals, setPeripherals] = useState(new Map());
+  const [connectedPeripheralId, setConnectedPeripheralId] = useState(null);
+  const [bleData, setBleData] = useState({ speed: 0, odo: 0, temperature: 0,batterystatus:0 });
+  const [dataStreamCount, setDataStreamCount] = useState(0);
 
   useEffect(() => {
     BleManager.start({ showAlert: false });
@@ -35,7 +39,22 @@ const App = () => {
     };
 
     requestBluetoothPermission();
-  }, []);
+
+    
+    const discoverPeripheralListener = BleManagerEmitter.addListener(
+      'BleManagerDiscoverPeripheral',
+      handleDiscoverPeripheral
+    );
+    const updateValueListener = BleManagerEmitter.addListener(
+      'BleManagerDidUpdateValueForCharacteristic',
+      handleUpdateValueForCharacteristic
+    );
+
+    return () => {
+      discoverPeripheralListener.remove();
+      updateValueListener.remove();
+    };
+  }, [peripherals]);
 
   const startScan = () => {
     if (!isScanning) {
@@ -53,54 +72,97 @@ const App = () => {
   const handleDiscoverPeripheral = (peripheral) => {
     if (peripheral.name) {
       setPeripherals(new Map(peripherals.set(peripheral.id, peripheral)));
+      console.log(peripheral,'device data')
     }
   };
 
-  useEffect(() => {
-    const discoverPeripheralListener = BleManagerEmitter.addListener(
-      'BleManagerDiscoverPeripheral',
-      handleDiscoverPeripheral
-    );
-
-    return () => {
-      discoverPeripheralListener.remove();
-    };
-  }, [peripherals]);
 
   const connectToPeripheral = (peripheralId) => {
-    console.log('pressed connect button');
+    console.log('id',peripheralId)
     BleManager.connect(peripheralId)
       .then(() => {
         console.log('Connected to ' + peripheralId);
+        setConnectedPeripheralId(peripheralId);
       })
       .catch((error) => {
         console.log('Connection error', error);
       });
   };
 
+  const disconnectFromPeripheral = (peripheralId) => {
+    BleManager.disconnect(peripheralId)
+      .then(() => {
+        console.log('Disconnected from ' + peripheralId);
+        setConnectedPeripheralId(null);
+      })
+      .catch((error) => {
+        console.log('Disconnection error', error);
+      });
+  };
+
+  const startNotification = (peripheralId) => {
+    const serviceUUID = '0000180f-0000-1000-8000-00805f9b34fb';  // Replace with your service UUID
+    const characteristicUUID = '3fe17662-9786-11eb-a8b3-0242ac130003';  // Replace with your characteristic UUID
+
+    BleManager.startNotification(peripheralId, serviceUUID, characteristicUUID)
+      .then(() => {
+        console.log('Notification started');
+      })
+      .catch((error) => {
+        console.log('Notification error', error);
+      });
+  };
+
+  const handleUpdateValueForCharacteristic = ({ value }) => {
+    const buffer = Buffer.from(value);
+    const data = buffer.toString();
+    handleBLEData(data);
+  };
+
+  const handleBLEData = (data) => {
+    const decodedData = data.split('|');
+    setBleData({
+      speed: parseInt(decodedData[0], 10),
+      odo: parseFloat(decodedData[1]),
+      temperature: parseInt(decodedData[2], 10),
+      batterystatus: parseInt(decodedData[3])
+    });
+    setDataStreamCount(prevCount => prevCount + 1);
+  };
+
   const renderItem = ({ item }) => (
     <View style={styles.peripheralItem}>
-      <View style={styles.peripheralInfo}>
-        <Text style={styles.peripheralName}>{item.name || 'Unnamed Device'}</Text>
-        <Text style={styles.peripheralId}>{item.id}</Text>
-      </View>
-      <Button
-        title="Connect"
-        onPress={() => connectToPeripheral(item.id)}
-        disabled={isScanning}
-        color="#1E90FF" // Bright button color
-      />
+    <View>
+      <Text style={styles.peripheralName}>{item.name || 'Unnamed Device'}</Text>
+      <Text style={styles.peripheralId}>{item.id}</Text>
     </View>
+    {connectedPeripheralId === item.id ? (
+      <Button title="Disconnect" onPress={() => disconnectFromPeripheral(item.id)} />
+    ) : (
+      <Button title="Connect" onPress={() => connectToPeripheral(item.id)} />
+    )}
+  </View>
   );
   return (
-    <View style={styles.container}>
-      <Button title="Start Scanning" onPress={startScan} color="#1E90FF"/>
-      <FlatList
-        data={Array.from(peripherals.values())}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <TouchableOpacity style={styles.scanButton} onPress={startScan}>
+      <Text style={styles.scanButtonText}>Start Scanning</Text>
+    </TouchableOpacity>
+    <FlatList
+      data={Array.from(peripherals.values())}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+    {connectedPeripheralId && (
+        <View style={styles.dataContainer}>
+          <Text style={styles.dataText}>Speed: {bleData.speed}</Text>
+          <Text style={styles.dataText}>ODO: {bleData.odo}</Text>
+          <Text style={styles.dataText}>Temperature: {bleData.temperature}</Text>
+          <Text style={styles.dataText}>Data Stream Count: {dataStreamCount}</Text>
+          <Text style={styles.dataText}>batteryStatus: {bleData.batterystatus} </Text>
+        </View>
+      )}
+  </SafeAreaView>
   );
 };
 
@@ -135,6 +197,30 @@ const styles = StyleSheet.create({
   peripheralId: {
     fontSize: wp('3.5%'),
     color: 'green', // Bright font color
+  },
+  scanButton: {
+    backgroundColor: '#007bff',
+    padding: wp('4%'),
+    borderRadius: wp('2%'),
+    alignItems: 'center',
+    marginBottom: hp('2%'),
+  },
+  scanButtonText: {
+    color: '#ffffff',
+    fontSize: wp('4%'),
+  },
+  dataContainer: {
+    marginTop: hp('2%'),
+    padding: wp('4%'),
+    backgroundColor: '#ffffff',
+    borderRadius: wp('2%'),
+    borderWidth: 1,
+    borderColor: '#dddddd',
+  },
+  dataText: {
+    fontSize: wp('4%'),
+    color: '#333333',
+    marginBottom: hp('1%'),
   },
 });
 
